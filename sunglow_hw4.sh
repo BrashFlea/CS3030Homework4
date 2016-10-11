84124649bcea10278de26beb30316e295f5a156c
Parker Jensen <parker.jensen@aggiemail.usu.edu>
2016-10-11 12:28:33 -0600

    Initial Commit

---
+++ sunglow_hw4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: sunglow_hw4.sh
+# 
+#         USAGE: ./sunglow_hw4.sh 
+# 
+#   DESCRIPTION: 
+# 
+#        AUTHOR: Parker Jensen (), parker.jensen@aggiemail.usu.edu
+#  ORGANIZATION: 
+#       CREATED: 10/11/2016 12:28
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+
+
+
+exit 0
+
