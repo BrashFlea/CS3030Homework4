d96f24f5f639588bfec0d264d65759eb665910e3
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2016-10-13 21:04:15 -0600

    Initial Commit

--- sunglow_hw4_1.sh
+++ sunglow_hw4_1.sh
@@ -86,5 +86,34 @@ then
 	wget icarus.cs.weber.edu/~hvalle/cs3030/MOCK_DATA_$year.tar.gz 
 fi
 
+#Jonathan Mirabile
+#step 4
+`mkdir -p temp`
+DATE=`date +%Y_%m_%d_%H:%M:%S`
+
+echo "Unpacking Files"
+
+`tar -xzvf MOCK_DATA_2015.tar.gz -C temp`
+`tar -xzvf MOCK_DATA_2016.tar.gz -C temp`
+
+echo "Switching Directories"
+`cd temp`
+
+echo "Processing Data"
+#Awk script does a couple things to the data:
+#0 cat the data in the directory based on number (1-10 at this point in time)
+#1 print all records (pipe into awk)
+#2 determine if the email field is null and replace it (pipe into awk)
+#3 determine if the record is a female from canada and print the first name, last name and email.
+#Note: this strips the headers in the process
+#5 pipe into new file (append) for zip
+for i in {1..10}
+do
+`cat MOCK_DATA[i].csv | 
+awk -F, '{print $2 "," $3 "," $4 "," $5 "," $6}' | 
+awk -F, '{OFS = FS}{if(length($3) == 0){$3="waldo@weber.edu"}print}' |
+awk -F, '{if($4 == "Female" && $5 == "Canada")print $1 "," $2 "," $3}' >>
+MOCK_DATA_FILTER_$DATE`
+done
 exit 0
 
